cec3ba51b9c7fe5079c95593f1e081b5f4de1a27
Pedro Silva <pedro.silva.bos@gmail.com>
2019-04-29 20:14:23 +0000

    adding new blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -11,7 +11,7 @@
 @section('content')
 <div class="bs-example">
   <!-- Button HTML (to Trigger Modal) -->
-  <a href="#myModal" class="btn btn-lg btn-primary" data-toggle="modal">Launch Demo Modal</a>
+  <a href="#myModal"  data-toggle="modal">Quake 3 die hard player</a>
 
   <!-- Modal HTML -->
   <div id="myModal" class="modal fade">
@@ -19,13 +19,13 @@
           <div class="modal-content">
               <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                  <h4 class="modal-title">YouTube Video</h4>
+                  <h4 class="modal-title">Quake 3 Ownage</h4>
               </div>
               <div class="modal-body">
-                  <iframe id="cartoonVideo" width="560" height="315" src="//www.youtube.com/embed/YE7VzlLtp-4" frameborder="0" allowfullscreen></iframe>
+                  <iframe id="quake3" width="640" height="480" src="//www.youtube.com/embed/c20CeqL2Dh0" frameborder="0" allowfullscreen></iframe>
               </div>
           </div>
       </div>
   </div>
-</div> 
+</div> s
 @endsection
