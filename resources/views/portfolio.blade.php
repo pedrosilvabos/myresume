80f431751ee4004b6564858525f53c1f388b227c
Pedro Silva <pedro.silva.bos@gmail.com>
2019-04-29 20:05:54 +0000

    adding new blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -9,5 +9,23 @@
 @endsection
 
 @section('content')
-    <p>This is my body content.</p>
+<div class="bs-example">
+  <!-- Button HTML (to Trigger Modal) -->
+  <a href="#myModal" class="btn btn-lg btn-primary" data-toggle="modal">Launch Demo Modal</a>
+
+  <!-- Modal HTML -->
+  <div id="myModal" class="modal fade">
+      <div class="modal-dialog">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                  <h4 class="modal-title">YouTube Video</h4>
+              </div>
+              <div class="modal-body">
+                  <iframe id="cartoonVideo" width="560" height="315" src="//www.youtube.com/embed/YE7VzlLtp-4" frameborder="0" allowfullscreen></iframe>
+              </div>
+          </div>
+      </div>
+  </div>
+</div> 
 @endsection
