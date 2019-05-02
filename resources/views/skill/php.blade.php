6b032ee2d9da23fba752a8f0a242a8882e170139
Pedro <pedro.silva.bos@gmail.com>
2019-05-02 00:26:58 +0000

    staging blank controllers

---
+++ resources/views/skill/php.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+<h1> Ruby on rails Section </h1>
+@endsection
